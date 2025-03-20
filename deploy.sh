6a5a13e11c34650ec883baab6cab5c61f439ca51
SURYA U S <166891789+SURYAULAGANATHAN@users.noreply.github.com>
2025-03-20 08:09:53 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u surya22csr214 -p dckr_pat_P8p515NO80pfzUgG_MymCOo5J64
+    docker tag test surya22csr214/devops
+    docker push surya22csr214/devops
+    # docker-compose up -d
     
