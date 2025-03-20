6aa3c4e0bfb755df444d63a7ab94d12e0f2c7e01
SURYA U S <166891789+SURYAULAGANATHAN@users.noreply.github.com>
2025-03-20 08:10:50 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u surya22csr214 -p dckr_pat_P8p515NO80pfzUgG_MymCOo5J64
-    docker tag test surya22csr214/devops
+    docker tag test1 surya22csr214/devops
     docker push surya22csr214/devops
     # docker-compose up -d
     
